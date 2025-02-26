43ca433f1765229b339aadedcc25a8b6c070ecb1
Hojun Kim <hojunkim02@icloud.com>
2025-02-26 20:50:15 +0900

    animation 2

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -9,12 +9,6 @@ public class BackgroundScroll : MonoBehaviour
     [Header("References")]
     public MeshRenderer meshRenderer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
     private void Update()
     {
         meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,10 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D PlayerRigidBody;
-
     public Animator PlayerAnimator;
 
     private bool isGrounded = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
